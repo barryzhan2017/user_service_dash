622bd747a97f1ba2ca0e8a5fa92cc7674b43db9b
zhanzhicheng <barryzhan2017@gmail.com>
2020-12-11 16:38:12 +0800

    migrate to elastic beanstalk user-service-api

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash
 import os
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-user_service_url = "http://18.216.73.6"
+user_service_url = "http://user-service.eba-txbhbpef.us-east-2.elasticbeanstalk.com/"
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=external_stylesheets)
 page_url = "http://user-service-dash.eba-y82cxuwr.us-east-2.elasticbeanstalk.com/"
